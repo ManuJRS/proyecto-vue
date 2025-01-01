e496e77cde4f5d85a42c9f5678bb6f296ac0acf6
Manu <manueljesusrejonsantana@gmail.com>
2024-12-31 18:09:28 -0600

    chore: added counter

---
+++ src/components/Contador.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1>Hola Mundo</h1>
+    <p>{{ contador }}</p>
+    <button @click="restar">Restar</button>
+    <button @click="incrementar">Incrementar</button>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const contador = ref(0);
+const incrementar = () => {
+    contador.value++;
+}
+const restar = () => {
+    contador.value--;
+}
+</script>
+
+<style scoped>
+p {
+    color: red;
+}
+</style>
